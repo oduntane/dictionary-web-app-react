0bf8af2f413bc642ff07ef87abeb2df48f68cce8
Emmanuel Oduntan <oduntane@icloud.com>
2025-08-27 11:20:22 +0100

    chore: add PlayButton

---
+++ src/components/buttons/PlayButton.jsx
@@ -0,0 +1,13 @@
+
+function PlayButton() {
+    return (
+        <button>
+            <span className="sr-only">Listen</span>
+            <span role="presentation" className="w-[3rem] aspect-square">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 75 75"><g fill="#A445ED" fill-rule="evenodd"><circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/><path d="M29 27v21l21-10.5z"/></g></svg>
+            </span>
+        </button>
+    )
+}
+
+export  default PlayButton
