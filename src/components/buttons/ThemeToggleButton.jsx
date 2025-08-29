eb476a72ca1c8d9c959994c388f7e36df3f0789f
Emmanuel Oduntan <oduntane@icloud.com>
2025-08-29 10:29:11 +0100

    fix: fix toggle button

--- src/components/buttons/ThemeToggleButton.jsx
+++ src/components/buttons/ThemeToggleButton.jsx
@@ -16,7 +16,7 @@ function ThemeToggleButton() {
                 }
             }}>
                 <span className="sr-only">Toggle Theme</span>
-                <span className={`block w-[0.875rem] aspect-square bg-white rounded-full ${theme === 'light' ? 'ml-0 mr-auto' : 'mr-0 ml-auto'}`}></span>
+                <span className={`block w-[0.875rem] aspect-square bg-white rounded-full ${theme === 'light' ? 'ml-0 mr-[calc(100%-0.875rem)]' : 'mr-0 ml-[calc(100%-0.875rem)]'} transition-all`}></span>
             </button>
             <span role="presentation" className="w-[1.25rem] aspect-square stroke-[#838383] dark:stroke-purple-500">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/></svg>
